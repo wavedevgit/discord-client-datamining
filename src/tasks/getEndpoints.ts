149937462b1c182c6419cb1669f5843aa29c177b
WaveDev <wavedev@github.com>
2026-02-04 15:07:07 +0100

    fix again

--- src/tasks/getEndpoints.ts
+++ src/tasks/getEndpoints.ts
@@ -8,7 +8,7 @@ export default async function getEndpoints(build: Build): Promise<void> {
     const chunks = await readdir('./build/chunks/');
 
     for (const chunkId of chunks) {
-        if (chunkId === 'readme.md') continue;
+        if (chunkId === 'readme.md' || chunkId === 'libdiscore') continue;
         const content = await chunk(chunkId.replace('.js', ''));
         // skip ones without a createLoader reference
         if (!content.includes('ME:"/users/@me",')) continue;
